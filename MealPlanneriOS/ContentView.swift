1b435226dbeb4273a62077ca9c35e35cacec37fc
theonam5 <theo.namor@hotmail.fr>
2025-09-14 09:59:28 +0200

    Initial Commit

---
+++ MealPlanneriOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MealPlanneriOS
+//
+//  Created by Théo Namor on 14/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MealPlanneriOS/MealPlanneriOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealPlanneriOSApp.swift
+//  MealPlanneriOS
+//
+//  Created by Théo Namor on 14/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct MealPlanneriOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
